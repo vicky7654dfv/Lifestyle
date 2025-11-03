c5ca743ab4766bbb9a9cc9c183da6ab6e7f75c5d
vicky7654dfv <vignesht2013@gmail.com>
2025-11-03 19:04:11 +0530

    ch

--- src/components/Banner1/Banner1.jsx
+++ src/components/Banner1/Banner1.jsx
@@ -3,18 +3,19 @@ import Style from "./Banner1.module.css";
 import { Link } from 'react-router-dom';
 
 export default function Banner1() {
-  const videoSrc = new URL('../../assets/Banner1/vidBg.mp4', import.meta.url).href;
+  // const videoSrc = new URL('../../assets/Banner1/vidBg.mp4', import.meta.url).href;
 
   return (
     <div data-aos="fade-up" className={Style.bannerContainer}>
       <video
-        src={videoSrc}
-        className={Style.backgroundVideo}
-        autoPlay
-        muted
-        loop
-        playsInline
-      />
+  src="/videos/vidBg.mp4"
+  className={Style.backgroundVideo}
+  autoPlay
+  muted
+  loop
+  playsInline
+/>
+
       <div className={Style.content}>
         <h1 className={Style.title}>LifeStyle</h1>
         <p className={Style.paragraph}>
